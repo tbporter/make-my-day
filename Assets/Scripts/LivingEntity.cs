8910481dffcb55d1dde72cca3d6ac74406634c2d
Travis <travis.porter@gmail.com>
2013-07-13 12:57:26 -0600

    Added monster type. Simple Monster AI. Integrated animation to movement

---
+++ Assets/Scripts/LivingEntity.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using System.Collections;
+
+public class LivingEntity : MonoBehaviour {
+	
+	protected Vector3 facingDirection;
+	public Vector3 startingDirection;
+	protected Vector3 moveDirection = Vector3.zero;
+	
+	public float gravity = 20.0f;
+	// Use this for initialization
+	void Start () {
+		
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
