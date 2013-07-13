05ced3b3b9184d96f1c9d5aa478540f0386c4280
Travis Porter <Travis@marisa.(none)>
2013-07-12 23:07:35 -0600

    simple movement

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+    public float speed = 6.0F;
+    public float jumpSpeed = 20F;
+    public float gravity = 25.0F;
+    private Vector3 moveDirection = Vector3.zero;
+    void Update() {
+        CharacterController controller = GetComponent<CharacterController>();
+        if (controller.isGrounded) {
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *= speed;
+            if (Input.GetButtonDown("Jump"))
+                moveDirection.y = jumpSpeed;
+            
+        }
+        moveDirection.y -= gravity * Time.deltaTime;
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+}
