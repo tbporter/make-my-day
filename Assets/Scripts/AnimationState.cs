3f694b6b39e107b19920fc010258649f744f6011
Eve Park <github@eve-park.com>
2013-07-13 12:04:51 -0600

    Added Animation Scripts

---
+++ Assets/Scripts/AnimationState.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using System.Collections;
+
+public class AnimationState : MonoBehaviour {
+	public bool isRunning = false;
+	public int Facing = 1;
+	public bool isGrounded;
+	public bool isShooting;
+	
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
