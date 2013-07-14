96aaffff8d6e1663750b4f1c59abcf726a6d182b
Travis <travis.porter@gmail.com>
2013-07-13 22:30:39 -0600

    finished harpies

--- Assets/Scripts/MobStatus.cs
+++ Assets/Scripts/MobStatus.cs
@@ -17,11 +17,11 @@ public class MobStatus : MonoBehaviour {
 		
 		if(collision.transform.tag == "Projectile"){
 			Destroy(collision.gameObject);
-			die();
+			die(collision.contacts[1].point);
 		}
 		
 	}
-	void die(){
+	void die(Vector3 hitLoc){
 		
 		int[,] positions = new int[,] { { -1,-1},{-1,0},{-1,1},{0,-1},{0,0},{0,1},{1,-1},{1,0},{1,1}};
 		
@@ -32,19 +32,13 @@ public class MobStatus : MonoBehaviour {
 		Destroy(gameObject);
 		
 		GameObject plane;
-		for(int i=0;i<positions.GetLength(0);i++){
-			//plane = GameObject.CreatePrimitive(PrimitiveType.Plane);
+		for(int i=0;i<positions.GetLength(0);i++){;
 			Vector3 pos = new Vector3((positions[i,0]*width*10)+middle.x,(positions[i,1]*height*10)+middle.y,0);	
 			
 			plane = (GameObject) Instantiate(shard,pos,shard.transform.rotation);
 			plane.transform.localScale = new Vector3(width,0,height);
-			//Rigidbody rb = plane.AddComponent<Rigidbody>();
-			//rb.mass = 1;
 			
-			plane.rigidbody.AddExplosionForce(700f,middle,10f,0f);
-			//plane.transform.Rotate(new Vector3(-90,0,0));
-			//plane.transform.position = new Vector3((positions[i,0]*width*10)+middle.x,(positions[i,1]*height*10)+middle.y,0);	
-			//plane.transform.position = new Vector3();
+			plane.rigidbody.AddExplosionForce(700f,hitLoc,10f,0f);
 		}
 
 		
