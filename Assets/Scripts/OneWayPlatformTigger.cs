a0c190345e07f17224c5566327d4d33ecf14a372
Travis <travis.porter@gmail.com>
2013-07-13 23:20:45 -0600

    powerups

--- Assets/Scripts/OneWayPlatformTigger.cs
+++ Assets/Scripts/OneWayPlatformTigger.cs
@@ -6,12 +6,10 @@ public class OneWayPlatformTigger : MonoBehaviour {
 	void OnTriggerEnter(Collider jumper){
 		Collider platform = transform.parent.collider;
 		Physics.IgnoreCollision (jumper.GetComponent<CharacterController>(),platform);
-		print ("enter trigger");
 	}
 	void OnTriggerExit (Collider jumper){
 		jumper.gameObject.layer = 0;
 		Collider platform = transform.parent.collider;
 		Physics.IgnoreCollision (jumper.GetComponent<CharacterController>(),platform,false);
-		print("exit trigger");
 	}
 }
