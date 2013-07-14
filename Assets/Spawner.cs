9f466e79ef7bb29cf4cd1896784e7b9b16e83af8
Travis <travis.porter@gmail.com>
2013-07-14 16:32:26 -0600

    Work around for the worst bug ever, thanks Unity team.

--- Assets/Spawner.cs
+++ Assets/Spawner.cs
@@ -3,13 +3,18 @@ using System.Collections;
 
 public class Spawner : MonoBehaviour {
 	public GameObject prefab;
+	public string prefabS;
 	// Use this for initialization
 	void Start () {
+		if(prefab == null)
+			prefab = (GameObject) Resources.Load(prefabS);
+			
 		Instantiate(prefab,transform.position,transform.rotation);
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		
 	}
 }
