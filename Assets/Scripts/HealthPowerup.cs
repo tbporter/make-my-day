927186d35602e17c249efd51a93d09fc556a2bc1
Eve Park <github@eve-park.com>
2013-07-14 17:24:19 -0600

    minor

--- Assets/Scripts/HealthPowerup.cs
+++ Assets/Scripts/HealthPowerup.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class HealthPowerup : Powerup {
 	ParticleSystem bubbles;
 	override public void getPwer(GameObject pickUper){
+		
 		bubbles = gameObject.transform.FindChild("Bubbles").GetComponent<ParticleSystem>();
 		bubbles.Play();
 		
